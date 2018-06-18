33b8102d7ce841bc98d8c2409320849811e316e3
EmirKayaZ <40175829+EmirKayaZ@users.noreply.github.com>
2018-06-19 00:35:11 +0300

    Update minimap.user.js

--- minimap.user.js
+++ minimap.user.js
@@ -7,8 +7,8 @@
 // @match        https://pixelzone.io/*
 // @match        http://pixelzone.io/*
 // @homepage     https://github.com/EmirKayaZ/T.r.Z.o.n.e/
-// @updateURL    https://raw.githubusercontent.com/T.r.Z.o.n.e/T-RKishArmyM/blob/master/minimap.user.js
-// @downloadURL  https://raw.githubusercontent.com/T.r.Z.o.n.e/T-RKishArmyM/blob/master/minimap.user.js
+// @updateURL    https://raw.githubusercontent.com/EmirKayaZ/T.r.Z.o.n.e/blob/master/minimap.user.js
+// @downloadURL  https://raw.githubusercontent.com/EmirKayaZ/T.r.Z.o.n.e/blob/master/minimap.user.js
 // @grant        none
 // ==/UserScript==
 
